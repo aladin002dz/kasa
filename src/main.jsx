2e893840b47dab17dde22c92b5695143101c9045
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-04 12:15:42 +0100

    Refactor Archi with React Router :  Root File (app changing) + main styles + call Routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from '@pages//root.jsx';
+import Home from '@pages/home/index.jsx';
+import About from '@pages/about/index.jsx';
+import Surprise from '@pages/surprise/index.jsx';
+import Annonce from '@pages/annonce/index.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />},
+        {
+          path: "/annonce",
+          element: <Annonce />},
+        {
+        path: "/benji",
+        element: <Surprise />}
+    ],
+  }
+   
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/root.jsx
@@ -0,0 +1,18 @@
+import {Outlet} from 'react-router-dom'
+import Header from "@components/Header/Header"
+import Footer from "@components/Footer/Footer"
+
+import '@root/styles.css'
+
+const Root = () => {
+
+
+  return (
+  <>
+   <Header />
+   <main className="content-first">{<Outlet />}</main>
+   <Footer />
+  </>  )
+}
+
+export default Root
