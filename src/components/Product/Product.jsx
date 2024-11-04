3eb0e67ee7ca56fbbcc6508c4fc40cb476ba5896
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-04 18:44:14 +0100

    Create Product Layout component + UI css + get single data to display

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,68 @@
+
+import './Product.css'
+
+
+const Product = (props) => {
+
+
+  const {details} = props
+
+
+function scrollHeight(e) {
+
+  if (e.target.classList.contains('panel-container')) {
+
+    let dynamicHeight = e.target.scrollHeight;
+
+    e.target.closest('.product-panel').style.setProperty('height',`${dynamicHeight}px`);
+  }
+
+}
+
+
+  return (
+
+    <article className='logement'>
+
+      <div className='side-left'>
+
+        <h3 className='product-name'>{details.title}</h3>
+        <a href={`https://www.google.fr/maps/place/${details.location}`} className='product-location'>{details.location}</a>
+
+        <div className='product-tags'>
+          {details.tags.map((tag,index) =>{
+            return (
+              <span key={`${tag}-${index}`} className='tag'>{tag}</span>
+            )
+          })}
+        </div>
+
+      </div>
+      <div className='side-right'>
+        <p className='product-owner'>{details.host.name}<img src={details.host.picture} alt={details.host.name} className='thumbnail'/></p>
+        {details.rating ? (<span className="star">Note : {`${details.rating} / 5`}</span>):'aucunes notes'}
+      </div>
+
+      <div className='side-bottom'>
+        <div className='product-panel'>
+          <h3 className='title'>Description </h3>
+          <p className='panel-container'>{details.description}</p>
+        </div>
+
+        <div className="product-panel" >
+          <h3 className='title'>Equipements</h3>
+          <ul className='panel-container'>
+          {details.equipments.map((equipement,index)=>{
+
+              return (<li key={`${equipement}-${index}`}>{equipement}</li>)
+              }) 
+            }
+          </ul>
+        </div>
+      </div>
+    </article>
+  
+  )
+}
+
+export default Product
--- src/pages/annonce/index.jsx
+++ src/pages/annonce/index.jsx
@@ -1,11 +1,18 @@
 import HeroBanner from "@components/Hero/Hero"
+import Product from "@components/Product/Product"
+
+import logement from '@data/logements.json'
 
 const Annonce = () => {
 
+  let details = logement[0]
 
   return (
     <>
-    <HeroBanner layout={'annonce'}/>
+      <HeroBanner layout={'annonce'}/>
+      <section className="logement-details">
+        <Product details={details} />
+      </section>
     </>
   )
 }
