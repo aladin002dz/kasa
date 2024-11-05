6699f9484ef0c94d45764a4b6aece481520c135c
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-05 14:40:30 +0100

    Add extra link Wrapping logo with Build In component Link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import logo from '@assets/kasa-logo.svg'
 import Navigation from '@components/Navigation/Navigation.jsx';
 import './Header.css'
@@ -7,10 +8,14 @@ const Header = () => {
 
   return (
     <header id="header" className='banner-header skeleton'>
-      <h1><img src={logo} alt="Logo du site Kasa" /></h1>
-      <Navigation />
+      <>
+        <Link to='/' aria-label="Lien Accueil du site Kasa" tabIndex="0">
+        <img src={logo} alt="Logo du site Kasa" />
+        </Link>
+        <Navigation />
+      </>
     </header>
-  );
+  )
 
 }
 
