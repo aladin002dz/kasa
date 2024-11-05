02dc4c54d2e0fd2547a6bc54cc83c0c5d8b909e3
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-05 12:36:20 +0100

    Add spacing Footer + Disable activeClassName attribut in Nav Component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,7 +5,7 @@ const Navigation = () => {
   return (
     <nav className="main-nav">
         <NavLink to="/" className='link nav-link'>Accueil</NavLink>
-        <NavLink to="/annonce" className='link nav-link' activeClassName="active">Single Annonce WIP</NavLink>
+        <NavLink to="/annonce" className='link nav-link' >Single Annonce WIP</NavLink>
         <NavLink to="/about" className='link nav-link'>A propos</NavLink>
         <NavLink to="/benji" className='link nav-link'>Surprise</NavLink>
         
