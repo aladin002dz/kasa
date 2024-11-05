0aabeb452a280233e2c0bc218acf01993d47a629
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-05 17:31:04 +0100

    Create Us component for Page About with Panel inside

---
+++ src/components/Us/Us.jsx
@@ -0,0 +1,23 @@
+import PanelTube from "@components/PanelTube/PanelTube";
+import './Us.css'
+
+const Us = () =>{
+
+  let softkills = ['Fiabilité','Responsabilite','Respect','Service','Sécurité'];
+
+  return (
+    <section className="us">
+      
+      {softkills.map((softkill,index) => {
+      
+        return(
+          <PanelTube key={`tube-${index}`} softkill={softkill} fullPanel={false} />
+        )
+
+      })}
+
+    </section>
+  )
+}
+
+export default Us
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,4 +1,7 @@
-import HeroBanner from "../../components/Hero/Hero"
+import HeroBanner from "@components/Hero/Hero"
+import Form from "@components/Form/Form"
+import Us from "@components/Us/Us"
+
 import "./About.css"
 
 const About = () => {
@@ -6,35 +9,7 @@ const About = () => {
 return(
   <>
     <HeroBanner layout={'about'}/>
-  
-    <div className="about-wrapper">
-      <form action="" className="side side-form">
-      <h3 className="main-title">On est là !</h3>
-      <div className="input-group">
-        <label htmlFor="name">Nom</label>
-        <input type="text" id="name" placeholder="Votre nom"/>
-      </div>
-        
-      <div className="input-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="Votre email"/>  
-      </div>
-        
-      <div className="input-group">
-          <label htmlFor="message">Message</label>
-          <textarea id="message" placeholder="Votre message"></textarea>
-      </div>
-
-      <button type="submit" className="btn btn-cta">Go !</button>
-      </form>
-
-      <div className="side side-prez">
-        <h3 className="main-title">Et ici aussi</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam eaque, voluptas provident tempora hic rem quas quidem error eligendi officia facere incidunt vel mollitia reprehenderit!</p>
-      </div>
-
-    </div>
-    
+    <Us/> 
   </>
 )
 
