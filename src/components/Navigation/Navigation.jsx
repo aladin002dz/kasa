3a9b4bdfab9693f5dc5f0a0176e2461dca018a52
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-05 17:30:07 +0100

    disable Surprise Link

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,8 +7,7 @@ const Navigation = () => {
         <NavLink to="/" className='link nav-link'>Accueil</NavLink>
         <NavLink to="/annonce" className='link nav-link' >Single Annonce WIP</NavLink>
         <NavLink to="/about" className='link nav-link'>A propos</NavLink>
-        <NavLink to="/benji" className='link nav-link'>Surprise</NavLink>
-        
+        {/* <NavLink to="/benji" className='link nav-link'>Surprise</NavLink> */}
     </nav>
   )
 } 
