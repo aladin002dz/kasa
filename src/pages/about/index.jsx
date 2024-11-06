4326381549a45bcbfa25c007a48518eec9679c5e
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-11-06 15:31:37 +0100

    Testing useLocation react Router + use Eeffect to display body special class on current page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -2,7 +2,7 @@ import HeroBanner from "@components/Hero/Hero"
 import Form from "@components/Form/Form"
 import Us from "@components/Us/Us"
 
-import "./About.css"
+import "./about.css"
 
 const About = () => {
 
--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -1,4 +1,5 @@
-import {Outlet} from 'react-router-dom'
+import {Outlet,useLocation} from 'react-router-dom'
+import { useEffect } from 'react';
 import Header from "@components/Header/Header"
 import Footer from "@components/Footer/Footer"
 
@@ -6,7 +7,27 @@ import '@root/styles.css'
 
 const Root = () => {
 
+  let location = useLocation();
+  
+  if(location.pathname.length <= 1) {
 
+      location.pathname = '/home';
+
+  }
+  
+  let currentLocation = location.pathname.replace('/','');
+  
+  useEffect(() => {
+
+    let target = document.body
+
+    target.classList.remove(...target.classList)
+    
+    target.classList.add(currentLocation);
+
+  },[currentLocation])
+
+  
   return (
   <>
    <Header />
